f3007ff263992f01ba66e2d38ddd70ae3873ecdc
Greg Bruno <greg.bruno@gmail.com>
2006-10-11 17:58:05 +0000

    touch ups

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -6,6 +6,6 @@ SUMMARY_COMPATIBLE	= $(VERSION)
 SUMMARY_MAINTAINER	= Rocks Group
 SUMMARY_ARCHITECTURE	= i386, x86_64, ia64
 
-ROLL_REQUIRES		= base hpc kernel web-server os1 os2
+ROLL_REQUIRES		= base hpc kernel web-server os1 os2 service-pack
 ROLL_CONFLICTS		=
 
