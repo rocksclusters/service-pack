dd061029d6670509c523be07ad5968eae98800f9
Mason Katz <mason.katz@gmail.com>
2010-08-13 20:52:45 +0000

    noarch (easier to install on web server)

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -3,10 +3,12 @@ NAME    		= roll-$(ROLL)-usersguide
 VERSION			= 5.3.1
 RELEASE 		= 0
 
+RPM.ARCH		= noarch
+
 SUMMARY_COMPATIBLE	= $(VERSION)
 SUMMARY_MAINTAINER	= Rocks Group
 SUMMARY_ARCHITECTURE	= i386, x86_64
 
-ROLL_REQUIRES		= base kernel web-server os1 os2 service-pack
+ROLL_REQUIRES		= base kernel service-pack
 ROLL_CONFLICTS		=
 
