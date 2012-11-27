745f976d9abcf40f8dcefb8cdec206d9bec16a89
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-11-27 00:49:12 +0000

    Copyright Storm for Emerald Boa

--- src/base-rocks-command/commands/set/password/plugin_mysql.py
+++ src/base-rocks-command/commands/set/password/plugin_mysql.py
@@ -1,13 +1,13 @@
-# $Id: plugin_mysql.py,v 1.4 2012/05/06 05:49:40 phil Exp $
+# $Id: plugin_mysql.py,v 1.5 2012/11/27 00:49:28 phil Exp $
 # 
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		         version 5.5 (Mamba)
-# 		         version 6.0 (Mamba)
+# 		         version 5.6 (Emerald Boa)
+# 		         version 6.1 (Emerald Boa)
 # 
-# Copyright (c) 2000 - 2012 The Regents of the University of California.
+# Copyright (c) 2000 - 2013 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -55,6 +55,9 @@
 # @Copyright@
 #
 # $Log: plugin_mysql.py,v $
+# Revision 1.5  2012/11/27 00:49:28  phil
+# Copyright Storm for Emerald Boa
+#
 # Revision 1.4  2012/05/06 05:49:40  phil
 # Copyright Storm for Mamba
 #
